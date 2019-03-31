ee8b227ab9d01e3601f30922dc39b68ff06596c4
SAHRAENI MAULIDA <49121673+SAHRAENIMAULIDA@users.noreply.github.com>
2019-03-31 22:17:20 +0800

    Add files via upload

---
+++ uts1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout<<"nama : SAHRAENI MAULIDA " <<endl;
+    cout<<"nim : 1829140002 " <<endl;
+    cout<<"kelas : TEKOM B "  <<endl;
+    cout<<"tahun : 2018" <<endl;
+
+}
